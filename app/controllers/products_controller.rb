723b34bc00e0f1e04b0c612e474115a06ca86350
dgwp-projects <zayneabraham1@gmail.com>
2017-03-27 10:50:29 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,30 @@
 class ProductsController < ApplicationController
+	def index 
+		@products = Product.all
+	end
 
+	def new
+		@product = Product.new
+	end
+	
+	def create
+		
+	end
+
+	def inventory
+		@product = Product.find(params[:id])
+		if @product.inventory || @product.inventory == 0 
+
+			var = 'true'
+		else 
+
+			var = 'false'
+		end
+		render plain: var
+	end
+
+	def description
+		@product = Product.find(params[:id])
+		render plain: @product.description
+	end
 end
---
+++ db/migrate/20170327141215_add_items_to_products.rb
@@ -0,0 +1,6 @@
+class AddItemsToProducts < ActiveRecord::Migration
+  def change
+  	add_column :products, :inventory, :integer
+  	add_column :products, :description, :text  	
+  end
+end
